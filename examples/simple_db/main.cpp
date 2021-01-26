605db1e16d4945be1a5052f2860bb153f0abd318
osydunenko <osydunenko@gmail.com>
2021-01-26 14:52:59 +0100

    Create table statement

--- examples/simple_db/main.cpp
+++ examples/simple_db/main.cpp
@@ -30,13 +30,13 @@ int main(int argc, char *argv[])
 {
     soci_wrapper::session::session_ptr_type session = soci_wrapper::session::connect("object.db");
 
-    soci_wrapper::ddl<person>::create_table(*session,
+    *session << soci_wrapper::ddl<person>::create_table(
         soci_wrapper::fields_query<person>::id = soci_wrapper::primary_key_constraint,
         soci_wrapper::fields_query<person>::name = soci_wrapper::not_null_constraint,
         soci_wrapper::fields_query<person>::rate = soci_wrapper::unique_constraint
     );
 
-    soci_wrapper::ddl<account>::create_table(*session,
+    *session << soci_wrapper::ddl<account>::create_table(
         soci_wrapper::fields_query<account>::id = soci_wrapper::foreign_key_constraint<person>(
             soci_wrapper::fields_query<person>::id),
         soci_wrapper::fields_query<account>::account_id = soci_wrapper::unique_constraint
